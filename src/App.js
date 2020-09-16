bc25662305cf1d4792ed210a1c18b4269c94cc8c
Khwajapasha <Khwajapasha141@gamil.com>
2020-09-16 10:04:30 -0700

    Navigation bar implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Navigation} from './Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p> This Is First App...</p>
-      </header>
-    </div>
+    <header className="App-header">
+      
+    <Navigation />
+    </header>
+  </div>
   );
 }
 
---
+++ src/MYComps.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const MyComps = () => {
+    return ( <div>This is my Comps</div> );
+}
