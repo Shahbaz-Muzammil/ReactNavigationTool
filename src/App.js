55425f0e262735602db06f2666260b1bbc2d2b21
Khwajapasha <Khwajapasha141@gamil.com>
2020-09-16 05:41:26 -0700

    Navigation Bar implementation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p> This Is First App...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
